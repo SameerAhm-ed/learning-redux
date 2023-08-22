4b61a7082f8cef8fee8a92cb2c7e04afd9c90998
Sameer Ahmed <sameer03054@gmail.com>
2023-08-23 01:01:04 +0500

    feat: Cart Page added

---
+++ app/cart/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <section>
+        <div>
+          <h1 className="text-6xl text-center">CART PAGE</h1>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default page;
